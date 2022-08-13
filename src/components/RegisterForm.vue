ecb8a2fd5cea0bb3ef1d2bb010e4dddf91d934a0
unknown <mohammedhossam7777@gmail.com>
2022-08-13 21:33:29 +0200

    validation of login and registarion forms

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,156 @@
+<template>
+  <!-- alert msg -->
+
+  <div
+    class="text-white text-center font-bold p-4 rounded mb-4"
+    v-if="reg_show_alert"
+    :class="reg_alert_variant"
+  >
+    {{ reg_alert_msg }}
+  </div>
+  <!-- form -->
+  <VeeForm
+    :validation-schema="schema"
+    v-on:submit="register"
+    :initial-values="userData"
+  >
+    <!-- Name -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Name</label>
+      <VeeField
+        name="name"
+        type="text"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Name"
+      />
+      <ErrorMessage class="text-red-600" name="name" />
+      <!-- :rule="{required:true}" -->
+      <!-- :rules="'required' " -->
+      <!-- this will generate a spane tag if there is an error -->
+    </div>
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <VeeField
+        name="email"
+        type="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Age -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Age</label>
+      <VeeField
+        name="age"
+        type="number"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      />
+      <ErrorMessage class="text-red-600" name="age" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <!-- vee-validate by default by5rog bara el validation w ytl3 errormsg wa7da 2wel ma yla2e 2wel error odamo, bs lw 3ayz 25le my3mlsh kda w y3de 3ala kol el errors yshohfa mara w7da yb2a lazm 2a2olo bails=false, w 23ml el kalam ele m3mo ta7t dah -->
+      <VeeField name="password" :bails="false" v-slot="{ field, errors }">
+        <input
+          type="password"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          placeholder="Password"
+          v-bind="field"
+        />
+        <div class="text-red-600" v-for="(error, index) in errors" :key="index">
+          {{ error }}
+        </div>
+      </VeeField>
+    </div>
+    <!-- Confirm Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Confirm Password</label>
+      <VeeField
+        name="confirm_password"
+        type="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Confirm Password"
+      />
+      <ErrorMessage class="text-red-600" name="confirm_password" />
+    </div>
+    <!-- Country -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Country</label>
+      <VeeField
+        name="country"
+        as="select"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      >
+        <option value="USA">USA</option>
+        <option value="Mexico">Mexico</option>
+        <option value="Germany">Germany</option>
+        <option value="China">china</option>
+      </VeeField>
+      <ErrorMessage class="text-red-600 block" name="country" />
+    </div>
+    <!-- TOS -->
+    <div class="mb-3 pl-6">
+      <VeeField
+        name="tos"
+        value="1"
+        type="checkbox"
+        class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+      />
+      <label class="inline-block">Accept terms of service</label>
+      <ErrorMessage class="text-red-600" name="tos" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      v-bind:disabled="reg_in_submission"
+    >
+      Submit
+    </button>
+  </VeeForm>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+  data() {
+    return {
+      schema: {
+        name: "required|min:3|max:100|alpha_spaces",
+        email: "required|min:3|max:100|email",
+        age: "required|min_value:18|max_value:100",
+        password: "required|min:6|max:100|excluded:password",
+        confirm_password: "confirmed:@password",
+        country: "required|country_excluded:China,Mexico",
+        tos: "tos",
+      },
+      //for vee-validate intial values(name must be the same name in schema)
+      userData: {
+        country: "USA",
+      },
+      //for alert msgs
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: "bg-blue-500",
+      reg_alert_msg: "Please wait! Your account is being created.",
+    };
+  },
+  methods: {
+    //vee-validate 2bl mtsht8l el onsubmit function deh, bt3ml b default validate el 2wal w lw kda tsht8lha lw msh kda tw2fha, w kaman y default bt3ml e.preventDefault()
+    register(values) {
+      this.reg_in_submission = true;
+      this.reg_show_alert = true;
+      this.reg_alert_variant = "bg-blue-500";
+      this.reg_alert_msg = "Please wait! Your account is being created.";
+
+      this.reg_alert_variant = "bg-green-500";
+      this.reg_alert_msg = "Success! Your account has been created.";
+      console.log(values);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,12 +3,18 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from "vee-validate";
 import {
   required,
   min,
   max,
+  email,
+  confirmed,
+  min_value as minValue,
+  max_value as maxValue,
   alpha_spaces as alphaSpaces,
+  not_one_of as excluded,
 } from "@vee-validate/rules";
 
 export default {
@@ -20,9 +26,46 @@ export default {
     //here we register rules globally because we will use them many times, but we can also regesiter them locally
 
     defineRule("required", required);
+    defineRule("tos", required);
     defineRule("min", min);
     defineRule("max", max);
     defineRule("alpha_spaces", alphaSpaces);
-    //second argument is a function that make the validation, and here we are using the default function required from the liberary
+    defineRule("email", email);
+    defineRule("min_value", minValue);
+    defineRule("max_value", maxValue);
+    defineRule("confirmed", confirmed);
+    defineRule("excluded", excluded);
+    defineRule("country_excluded", excluded);
+
+    //second argument is a function that make the validation, and here we are using the default function required from the liberary. we can override the default functions, but there are some limitations, we can only override error msgs from global rules, also we can only ovrride functions that return false if error esixts
+
+    configure({
+      //the generateMessage functoin will be called every time a gloval validation function returns false
+      generateMessage: (ctx) => {
+        const msgs = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          min_value: `The field ${ctx.field} is too low.`,
+          max_value: `The field ${ctx.field} is too high.`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}.`,
+          country_excluded:
+            "Due to restrictions, we do not accept users from this location.",
+          confirmed: "The passwords don't match.",
+          tos: "You must accept the Terms of Service.",
+        };
+        const message = msgs[ctx.rule.name]
+          ? msgs[ctx.rule.name]
+          : `The field ${ctx.field} is invalid.`;
+
+        return message;
+      },
+      validateOnBlur: true, //defaults to true
+      validateOnChange: true, //defaults to true
+      validateOnInput: false, //defaults to false
+      validateOnModelUpdate: true, //defaults to true
+    });
   },
 };
